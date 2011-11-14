1a158e32de8d0b0f63483c7d17e5f0bd1533079a
ToonTalk <ToonTalk@dea441fb-b2e9-1f59-5e44-3a9161b638bd>
2011-11-14 17:14:07 +0000

    Processing of bindings and simple expressions working now.

--- trunk/src/uk/ac/ox/oucs/eyeondata/server/ServerUtilities.java
+++ trunk/src/uk/ac/ox/oucs/eyeondata/server/ServerUtilities.java
@@ -3,7 +3,18 @@
  */
 package uk.ac.ox.oucs.eyeondata.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import uk.ac.ox.oucs.eyeondata.server.objectify.DAO;
 
@@ -43,6 +54,44 @@ public class ServerUtilities {
     
     public static DAO getDao() {
         return dao;
+    }
+    
+    public static String[] fetchURLContents(String urlString, HttpServletRequest request) {
+	// returns String[2] where [0] is the contents of the url
+	// and [1] is any warnings or error messages to be sent to the client
+	urlString = urlString.replaceAll("&amp;", "&");
+	String result[] = new String[2];
+	try {
+	    URL url = new URL(urlString);
+	    URLConnection connection = url.openConnection();
+	    // set timeout to GAE of 10 seconds
+	    connection.setConnectTimeout(10000); 
+	    connection.setReadTimeout(10000);
+	    String userAgent = request.getHeader("user-agent");
+	    connection.setRequestProperty("User-Agent", userAgent);
+	    InputStream inputStream = connection.getInputStream();
+	    result[0] = inputStreamToString(inputStream);
+	    if (result[0].contains("404 Not Found")) {
+		// should be caught below and cache used
+		result[1] = "404 Not found returned from " + urlString;		
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    result[1] = e.toString();
+	}
+	return result;
+    }
+
+    public static String inputStreamToString(InputStream inputStream)
+	    throws IOException {
+	BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+	StringBuilder contents = new StringBuilder();
+	String line;
+	while ((line = in.readLine()) != null) {
+	    contents.append(line + "\r");
+	}
+	in.close();
+	return contents.toString();
     }
 
 }
